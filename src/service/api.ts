ee7b8c4cc9043c225979541daa2a60ad19f93d9b
Caian R. Ertl <hi@caian.org>
2020-05-24 02:47:37 -0300

    feat: api invocation

---
+++ src/service/api.ts
@@ -0,0 +1,69 @@
+import * as AWS from 'aws-sdk';
+import { Lambda } from 'aws-sdk';
+import { IHash, IPushEvent } from '../lib/types';
+
+export class TinyCIAPI
+{
+    private lambdaName: string;
+    private lambda: Lambda;
+
+    public constructor(lambdaName: string)
+    {
+        this.lambdaName = lambdaName;
+        this.lambda = new AWS.Lambda();
+    }
+
+    private makeAPIGatewayPayload(method: string, resource: string, body: string | null): IHash
+    {
+        return {
+            resource: '/{proxy+}',
+            path: `/${resource}`,
+            httpMethod: method,
+            headers:  {
+                'Content-Type': 'application/json'
+             },
+            queryStringParameters: null,
+            multiValueQueryStringParameters: null,
+            pathParameters: {
+              proxy:  `/${resource}`
+            },
+            stageVariables: {},
+            requestContext: {
+               resourcePath: '/{proxy+}',
+               httpMethod: method,
+               path:  `/v1/${resource}`,
+               protocol: 'HTTP/1.1',
+               stage: 'v1',
+            },
+            body,
+            isBase64Encoded: body !== null,
+        };
+    }
+
+    private async invoke(payload: any): Promise<Error | null>
+    {
+        const params = {
+            FunctionName: this.lambdaName,
+            InvocationType: 'Event',
+            LogType: 'None',
+            Payload: JSON.stringify(payload)
+        };
+
+        try {
+            await this.lambda.invoke(params).promise();
+        }
+        catch (e) {
+            return e;
+        }
+
+        return null;
+    }
+
+    public async notifyPushEvent(event: IPushEvent): Promise<void>
+    {
+        const body = Buffer.from(JSON.stringify(event)).toString('base64');
+        const payload = this.makeAPIGatewayPayload('POST', 'pipeline', body);
+
+        await this.invoke(payload);
+    }
+}
