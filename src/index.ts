6195c05da1be1dbd5c7157fa98d54d1da58f7e14
Caian R. Ertl <hi@caian.org>
2020-05-24 02:48:18 -0300

    feat: put everything together

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,45 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 import { GithubWebhookSchemaValidator } from './schema/githubWebhook';
-import logger from './lib/logger';
-import parseWebhookBody from './parser';
-
-const log = logger({ isDebug: true });
-
-function response(statusCode: number): APIGatewayProxyResult
-{
-    return { body: '', statusCode };
-}
+import { TinyCIAPI } from './service/api';
+import { logger } from './lib/logger';
+import { Environment, RequiredEnv } from './lib/environment';
+import { response, ExErr } from './lib/response';
+import { filterWebhookBody } from './filter';
 
 export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult>
 {
-    let body: any = null;
-    try {
-        body = JSON.parse(event.body!);
-    }
-    catch (e) {
-        log.error('could not parse event body', e);
-        return response(500);
-    }
+    const environment = new Environment();
+    const isDebug = Boolean(environment.get(RequiredEnv.IsDebug));
+    const log = logger({ isDebug });
 
-    const validator = new GithubWebhookSchemaValidator(body);
-    if (!validator.isValid()) {
-        log.error('received event body is invalid');
-        validator.getErrors().forEach((error: string): void => {
+    const body = ((): object | null => {
+        try {
+            return JSON.parse(event.body!);
+        }
+        catch (e) {
+            log.error(ExErr.UnparsableEventBody, e);
+            return null;
+        }
+    })();
+
+    if (body === null)
+        return response(400, ExErr.UnparsableEventBody);
+
+    const githubPayload = new GithubWebhookSchemaValidator(body);
+
+    if (!githubPayload.isValid()) {
+        log.error(ExErr.InvalidEventBody);
+        githubPayload.getErrors().forEach((error: string): void => {
             log.error(`- ${error}`);
         });
 
-        return response(500);
+        return response(400, ExErr.InvalidEventBody);
     }
 
-    console.log(parseWebhookBody(body));
+    const lambdaName = environment.get(RequiredEnv.TinyCIApiFunctionName) as string;
+    const api = new TinyCIAPI(lambdaName);
+
+    await api.notifyPushEvent(filterWebhookBody(body));
     return response(200);
 }
